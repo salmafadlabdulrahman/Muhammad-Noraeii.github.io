cb3eb9fbfb89485c23ef58abd1264411ce3b9a41
Muhammad Noraeii <Muhammad.Noraeii@gmail.com>
2025-01-31 23:01:42 +0330

    Extracted Java Script from index
    -Extracted Java Script from index

--- Script.js
+++ Script.js
@@ -1,30 +1,47 @@
-// گرفتن تمام گواهی‌ها
-const certificates = document.querySelectorAll('.certificate');
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.querySelectorAll('.dropdown');
+    dropdown.forEach(item => {
+        let timeout;
 
-// گرفتن modal و عناصر داخل آن
+        item.addEventListener('mouseenter', () => {
+            
+            clearTimeout(timeout);
+            const dropdownMenu = item.querySelector('.dropdown-menu');
+            dropdownMenu.style.display = 'block'; 
+        });
+
+        item.addEventListener('mouseleave', () => {
+            const dropdownMenu = item.querySelector('.dropdown-menu');
+            
+            timeout = setTimeout(() => {
+                dropdownMenu.style.display = 'none';
+            }, 500); 
+        });
+    });
+});
+
+const certificates = document.querySelectorAll('.certificate');
 const modal = document.getElementById('certificateModal');
 const modalImage = document.getElementById('modalImage');
 const caption = document.getElementById('caption');
 const closeModal = document.querySelector('.close');
 
-// اضافه کردن event listener برای هر تصویر گواهی
-certificates.forEach(cert => {
-    cert.addEventListener('click', () => {
-        const imgSrc = cert.querySelector('img').src; // گرفتن لینک تصویر
-        modal.style.display = "block"; // نمایش modal
-        modalImage.src = imgSrc; // قرار دادن تصویر در modal
-        caption.textContent = cert.querySelector('p').textContent; // نمایش عنوان گواهی در caption
+    certificates.forEach(cert => {
+        cert.addEventListener('click', () => {
+       
+            const imgSrc = cert.querySelector('img').src; 
+            modal.style.display = "block"; 
+            modalImage.src = imgSrc; 
+            caption.textContent = cert.querySelector('p').textContent; 
+         });
     });
-});
 
-// بستن modal وقتی کاربر روی دکمه × کلیک می‌کند
-closeModal.addEventListener('click', () => {
-    modal.style.display = "none";
-});
+    closeModal.addEventListener('click', () => {
+       modal.style.display = "none";
+    });
 
-// بستن modal وقتی کاربر خارج از modal کلیک می‌کند
-window.addEventListener('click', (e) => {
-    if (e.target === modal) {
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
         modal.style.display = "none";
-    }
-});
+         }
+    });
